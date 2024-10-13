5e0931e356208278e0871b7e689e40ff6190cb8f
SamuelLo <samuello300@gmail.com>
2024-10-12 17:19:49 -0700

    starting frontend

--- app/index.tsx
+++ app/index.tsx
@@ -3,13 +3,23 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      className="flex-col items-center justify-center bg-gray-100 w-full"
     >
-      <Text>Starting Page</Text>
+      <Text className="flex-1 items-center justify-center text-2xl text-blue-500">Worth It</Text>
+      <View
+        className="border-2 border-gray-200 rounded-lg p-4 w-full"
+      > 
+        <Text
+          className="text-xl font-semibold"
+        >
+          Price
+        </Text>
+        <Text
+          className="text-lg"
+        >
+          $1.00
+        </Text>
+      </View> 
     </View>
   );
 }
